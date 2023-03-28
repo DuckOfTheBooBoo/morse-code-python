154ade77f5f8d1aad09ea2534b713ff527c7da8c
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-03-28 17:14:21 +0700

    Added morse_to_char function and char_to_morse function
    morse_to_char function takes an input of morse code string, iterate one by one then converts the code into char based on MORSE_TO_CHAR dictionary.
    char_to_morse function takes an input of string, checks if the string contains uppercase letter, if true, it convert the string into lowercase, then iterate one by one to convert it to morse code based on CHAR_TO_MORSE dictionary.

--- morse_code.py
+++ morse_code.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 MORSE_TO_CHAR = {
     '.-': 'a',
     '-...': 'b',
@@ -75,12 +77,41 @@ CHAR_TO_MORSE = {
     '0': '-----'
 }
 
-def main():
-    text = "Nama Saya Arajdian Altaf."
+def char_to_morse(text):
+
+    if any(ch.isupper() for ch in text):
+        text = text.lower()
+
+    morse_code = []
 
     for char in text:
         
         if char in CHAR_TO_MORSE.keys():
+            morse_code.append(CHAR_TO_MORSE[char])
+            morse_code.append(" ")
+
+        elif char == " ":
+            morse_code.append(" ")
+    
+    morse_code = ''.join(morse_code)
+
+
+def morse_to_char(morse_code):
+    text = []
+
+    for m_code in morse_code:
+
+        if m_code in MORSE_TO_CHAR.keys():
+            text.append(MORSE_TO_CHAR[m_code])
+        
+        elif m_code == "":
+            text.append(" ")
+    
+
+    print(''.join(text))
+
+def main():
+    pass
             
 
 if __name__ == "__main__":
