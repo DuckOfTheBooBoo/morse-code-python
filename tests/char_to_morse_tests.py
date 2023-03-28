4d579a556559d22c327c95e6b3c3dec5df251f16
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-03-28 17:35:30 +0700

    Added test todos in test cases

--- tests/char_to_morse_tests.py
+++ tests/char_to_morse_tests.py
@@ -1,1 +1,10 @@
-    
+"""
+TODO: 
+- Test that the first function can correctly convert a simple text string to a Morse code string.
+- Test that the first function can correctly handle strings containing uppercase letters.
+- Test that the first function can correctly handle special characters, such as punctuation marks.
+- Test that the first function can handle empty strings and return an empty string.
+- Test that the first function can handle strings containing non-alphabetic characters (such as numbers or symbols) and return an error message or raise an exception.
+- Test that the first function can handle input strings that are very long and return a Morse code string that is also very long.
+- Test that the first function can handle strings containing multiple lines or paragraphs and return a Morse code string with proper spacing.
+"""
