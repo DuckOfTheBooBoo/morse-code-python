6d71137c0e751ab06572528051f732248c9c1fc0
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-03-30 11:16:21 +0700

    Added audio for word space and char space in morse code sequence.

--- src/play_morse.py
+++ src/play_morse.py
@@ -6,25 +6,35 @@ import os
 
 DASH_PATH = os.path.join("..", "raw", "dash.wav")
 DOT_PATH = os.path.join("..", "raw", "dot.wav")
+CHAR_SPACE_PATH = os.path.join("..", "raw", "char_space.wav")
+WORD_SPACE_PATH = os.path.join("..", "raw", "word_space.wav")
 
 DOT = AudioSegment.from_wav(DOT_PATH)
 DASH = AudioSegment.from_wav(DASH_PATH)
+CHAR_SPACE = AudioSegment.from_wav(CHAR_SPACE_PATH)
+WORD_SPACE = AudioSegment.from_wav(WORD_SPACE_PATH)
 
 morse_code_sounds = {
     ".": DOT,
     "-": DASH,
+    " ": CHAR_SPACE,
+    " / ": WORD_SPACE
 }
 
 def main():
     test = ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
     morse_audio = AudioSegment.empty()
 
-    for char in test:
-        if char in morse_code_sounds:
-            sound = morse_code_sounds[char]
-            morse_audio += sound
+    test_mod = test.split(" / ")
+
+    for sequence in test:
+        for code in sequence:
+            if code in morse_code_sounds:
+                sound = morse_code_sounds[code]
+                # morse_audio += sound
+                play(sound)
             
-    play(morse_audio)
+    # play(morse_audio)
 
 if __name__ == "__main__":
     main()
