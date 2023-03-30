8787a7beb4657f5cc95c3f87ba09509b2f1220f1
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-03-30 19:35:17 +0700

    Use pydub for morse sound instead of raw audio.
    Removed everything inside raw directory.

--- src/play_morse.py
+++ src/play_morse.py
@@ -2,39 +2,55 @@ from morse_code import morse_to_text, text_to_morse
 from time import sleep
 from pydub import AudioSegment
 from pydub.playback import play
+from pydub.generators import Sine
 import os
 
-DASH_PATH = os.path.join("..", "raw", "dash.wav")
-DOT_PATH = os.path.join("..", "raw", "dot.wav")
-CHAR_SPACE_PATH = os.path.join("..", "raw", "char_space.wav")
-WORD_SPACE_PATH = os.path.join("..", "raw", "word_space.wav")
+DOT = Sine(500).to_audio_segment(duration=50)
+DASH = Sine(500).to_audio_segment(duration=150)
+INTRA_CHAR_SPACE = AudioSegment.silent(duration=50)
+INTER_CHAR_SPACE = AudioSegment.silent(duration=150)
+WORD_SPACE = AudioSegment.silent(duration=350)
 
-DOT = AudioSegment.from_wav(DOT_PATH)
-DASH = AudioSegment.from_wav(DASH_PATH)
-CHAR_SPACE = AudioSegment.from_wav(CHAR_SPACE_PATH)
-WORD_SPACE = AudioSegment.from_wav(WORD_SPACE_PATH)
-
-morse_code_sounds = {
-    ".": DOT,
-    "-": DASH,
-    " ": CHAR_SPACE,
-    " / ": WORD_SPACE
-}
-
-def main():
-    test = ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+def play_morse():
+    test = "-.- --.."
     morse_audio = AudioSegment.empty()
 
-    test_mod = test.split(" / ")
+    audio_segment_visual = []
 
-    for sequence in test:
-        for code in sequence:
-            if code in morse_code_sounds:
-                sound = morse_code_sounds[code]
-                # morse_audio += sound
-                play(sound)
+    if " / " in test:
+        sequences = test.split(" / ")
+    else:
+        sequences = [test,]
+
+    for word_index, word in enumerate(sequences):
+        letters = word.split(" ")
+
+        for char_index, char in enumerate(letters):
+            for symbol_index, symbol in enumerate(char):
+                
+                if symbol == ".":
+                    morse_audio += DOT
+                    audio_segment_visual.append("DOT")
+                elif symbol == "-":
+                    morse_audio += DASH
+                    audio_segment_visual.append("DASH")
+                
+                if symbol_index != len(char) - 1:
+                    # Add silence between characters
+                    morse_audio += INTRA_CHAR_SPACE
+                    audio_segment_visual.append("INTRA_CHAR_SPACE")
+                else:
+                    # Add silence between letters
+                    morse_audio += INTER_CHAR_SPACE
+                    audio_segment_visual.append("INTER_CHAR_SPACE")
+        
+        if word_index == len(sequences) - 1:
+            # Add silence between words
+            morse_audio += WORD_SPACE   
+            audio_segment_visual.append("WORD_SPACE")
             
     # play(morse_audio)
+    return audio_segment_visual
 
 if __name__ == "__main__":
-    main()
+    play_morse()
