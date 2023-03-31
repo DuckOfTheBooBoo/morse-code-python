d2f89aabf8572f6aef12a1987af5c0a0de6a311b
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2023-03-31 15:09:52 +0000

    Check if PyAudio and desktop exist or not.
    If PyAudio is not installed, morse code won't be played, and ask the user to save the file without tkinter filedialog if desktop is not found.

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from tkinter.filedialog import asksaveasfilename
-from pydub.playback import play
+from _tkinter import TclError
 from rich.console import Console
 from rich.prompt import Prompt, Confirm
 from src.morse_code import morse_to_text, text_to_morse
-from src.play_morse import play_morse
 from src.morse_code_error import MorseCodeError, MorseCodeNotFound
+from os.path import join
+from os import getcwd
 
+# Constants
 console = Console()
+PYAUDIO_EXIST = None
+
+try:
+    import pyaudio
+except ModuleNotFoundError:
+    console.print("[yellow]PyAudio is not installed. Morse code cannot be played.[/]")
+    PYAUDIO_EXIST = False
+
+else:
+    PYAUDIO_EXIST = True
+    from pydub.playback import play
+    from src.play_morse import play_morse
+
 
 def main():
     console.print("[green]Python Morse Code Translator[/]")
@@ -21,8 +36,10 @@ def main():
         try:
             result = text_to_morse(text)
             console.print(result)
-            audio_file = play_morse(result)
-            play(audio_file)
+
+            if PYAUDIO_EXIST:
+                audio_file = play_morse(result)
+                play(audio_file)
 
         except MorseCodeNotFound as e:
             console.print("[red]Exception[/]", e)
@@ -31,9 +48,38 @@ def main():
             ask_save = Confirm.ask(prompt="Save audio?")
 
             if ask_save:
-                file = asksaveasfilename(initialfile="audio.wav", defaultextension=".wav", filetypes=[("Audio File", ".wav")])
                 
-                audio_file.export(file, format="wav")
+                file = ""
+
+                try:
+                    file = asksaveasfilename(initialfile="audio.wav", defaultextension=".wav", filetypes=[("Audio File", ".wav")])
+
+                except TclError:
+                    
+                    console.print("[yellow]No desktop found. This most likely because you ran it in a terminal.[/]")
+
+                    while True:
+                        input_filename = Prompt.ask(prompt="File path: ", default="audio.wav")
+
+                        if not input_filename.endswith(".wav"):
+                            input_filename += ".wav"
+
+                        current_path = getcwd()
+
+                        console.log(join(current_path, input_filename))
+
+                        file_path_confirm = Confirm.ask("Correct?")
+
+                        if file_path_confirm:
+                            file = join(current_path, input_filename)
+                            break
+                        else:
+                            pass
+                
+                try:
+                    audio_file.export(file, format="wav")
+                except PermissionError as e:
+                    console.print(f"Error {e}")
 
             else:
                 pass
