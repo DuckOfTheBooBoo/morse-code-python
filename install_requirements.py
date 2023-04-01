3825ccdd715e1d372594fcb2f28c7ce52a54db2b
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2023-04-01 06:16:05 +0000

    Iterate over every package in requirements.txt
    Install each iteration one by one

--- install_requirements.py
+++ install_requirements.py
@@ -1,10 +1,14 @@
 import subprocess
 
+def pip_install(package):
+    subprocess.check_call(["pip", "install", package])
+
 def install_requirements():
     try:
         with open("requirements.txt") as f:
-            requirements = f.read().splitlines()
-        subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
+            for line in f:
+                pip_install(line)
+                
     except subprocess.CalledProcessError:
         print("Error installing requirements. Some packages may not be available. This maybe because your desktop doesn't have a GUI.")
 
